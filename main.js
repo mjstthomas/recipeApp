10cb32117abbf86d1656217b374953d80565ba08
Mathew <mjstthomas0516@gmail.com>
2020-02-29 16:18:39 -0700

    fixed keys

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 console.log('connected')
 
-let api_id = '846a99b2';
-let api_key = '75102f60b6053ece0d305d66405f78ec';
-
 function getRecipes(){
-	fetch(`https://api.edamam.com/search?q=chicken&app_id=${api_id}&app_key=${api_key}`)
+	let api_id = '846a99b2';
+	let api_key = '75102f60b6053ece0d305d66405f78ec	—';
+	let cuisine = $('#cuisine').val();
+	let diet = $('#diet').val();
+	fetch(`https://api.edamam.com/search?app_id=${api_id}&app_key=${api_key}&q=chicken&cuisineType=${cuisine}&diet=${diet}`)
 	.then(response => response.Json())
 	.then(responseJson => console.log(responseJson))
 	.catch(error => console.log(error))
 }
+
+function submit(){
+	$('#recipeForm').submit(event => {
+		event.preventDefault();
+		getRecipes()
+	})
+}
+
+$(submit)
