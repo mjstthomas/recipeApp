59b9f2deb1d63b76fe6f50e72fda6b9bd82ecfd2
Mathew <mjstthomas0516@gmail.com>
2020-03-01 20:38:32 -0700

    changed api and fixed the call

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 console.log('connected')
 
 function getRecipes(){
-	let api_id = '846a99b2';
-	let api_key = '75102f60b6053ece0d305d66405f78ec	—';
+	let api_key = '849d4e84cfcd41858d9dda42ac775fb2';
 	let cuisine = $('#cuisine').val();
 	let diet = $('#diet').val();
-	fetch(`https://api.edamam.com/search?app_id=${api_id}&app_key=${api_key}&q=chicken&cuisineType=${cuisine}&diet=${diet}`)
-	.then(response => response.Json())
+	let enteredIngredients =  $('#ingredients').val();
+	let ingredientsArr = enteredIngredients.split(' ');
+	let ingredients = ingredientsArr.join('')
+	fetch(`https://api.spoonacular.com/recipes/complexSearch?includeIngredients=${ingredients}&fillIngredients=true&cuisine=${cuisine}&diet=${diet}&instructionsRequired=true&addRecipeInformation=true&apiKey=${api_key}`)
+	.then(response => response.json())
 	.then(responseJson => console.log(responseJson))
 	.catch(error => console.log(error))
 }
