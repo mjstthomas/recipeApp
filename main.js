a7439e40d4a7ecae46630c86a3db8bd4c69119eb
Mathew <mjstthomas0516@gmail.com>
2020-03-08 20:42:21 -0700

    set up shopping list

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 
+//create an object to store data from recipe for shopping api
+const ingredientsLister = {
+	missedIngredients: []
+};
+
 const ingSearchString = str => {
     let arr = str.replace(/(,\s*)+/, ',');
     arr = arr.replace(/(and\s*)+/, "");
@@ -6,7 +11,7 @@ const ingSearchString = str => {
     let lastArr = newArr.join(',');
     return lastArr;
 }
-
+//pulls recipes from the spoonacular api based on search values and runs renderRecipe()
 function getRecipes(){
 	let api_key = '849d4e84cfcd41858d9dda42ac775fb2';
 	let cuisine = $('#cuisine').val();
@@ -28,9 +33,11 @@ function getRecipes(){
 	formReset()
 }
 
+//listens for submit button to be pressed and empties each section and runs getRecipe function
 function submit(){
 	$('#recipeForm').submit(event => {
 		event.preventDefault();
+		ingredientsLister.missedIngredients = [];
 		emptyDisp()
 		$('.navBar').toggleClass('shut')
 		$('.navBar').toggleClass('open')
@@ -42,6 +49,8 @@ function submit(){
 		$('footer').removeClass('hidden')
 	})
 }
+//Event listeners for the slider, start, and retry buttons
+
 
 	$('#showcase').on('click', "#starter", event => {
 		$('#showcase').empty()
@@ -86,6 +95,12 @@ function renderRecipe(obj){
 	})
 	// print url
 	$('footer').append(`<a href=${obj.results[rand].sourceUrl}`)
+
+	//fill array with missed ingredients
+		obj.results[rand].missedIngredients.forEach(name => {
+			ingredientsLister.missedIngredients.push(name.name)
+			console.log(ingredientsLister)
+		})
 }
 
 
@@ -103,8 +118,53 @@ function formReset(){
 	$('#diet').val('')
 }
 
+//renders the error message window
+
 function noResults(){
 	$('.oops').toggleClass('hidden')
 }
+
+
+//Shopping list Functions
+
+//listen for click on shopping cart to grow shopping section and fix remove hidden class from ul
+$('.cart').on('click', event=> {
+	$('.shopping').toggleClass('shoppingList')
+	$('.shopper').toggleClass('hidden')
+	$('.shopper').empty()
+	listMissedIngredients(ingredientsLister)
+})
+
+//fetch each item in missedIngredients array
+function listMissedIngredients(obj){
+	for (let i = 0; i < obj.missedIngredients.length; i++){
+		let foodString = obj.missedIngredents[i].split(" ").join("%20")
+		fetch(`https://google-shopping.p.rapidapi.com/search?language=EN&keywords=${foodString}&country=US`, {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "google-shopping.p.rapidapi.com",
+			"x-rapidapi-key": "d253257eddmshc83c0e4eacc6000p1d5b98jsn5bb91cdbab01"
+			}
+		})
+		.then(response => response.json())
+		.then(responseJson => {
+			listRender(responseJson)
+		})
+		.catch(err => {
+			console.log(err);
+		});
+		}
+	}
+//print each missed ingredient to the list
+function listRender(arr){
+	let i = 0;
+	if (obj.i.price >= 10){
+		i++
+	} else {
+		$('.shopper').append(`<li>${obj.i.title} ${obj.i.currency}${obj.i.price}</li>`)
+	}
+}
+
+
 submit()
 
